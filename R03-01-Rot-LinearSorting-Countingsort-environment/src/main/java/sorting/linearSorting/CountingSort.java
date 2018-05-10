e2463d601a3543bca4e729e9535d33a750fe654c
AthilaMatheusBorges <athila.borges@ccc.ufcg.edu.br>
2018-05-10 13:48:41 -0300

    00

--- R03-01-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/CountingSort.java
+++ R03-01-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/CountingSort.java
@@ -12,8 +12,47 @@ public class CountingSort extends AbstractSorting<Integer> {
 
 	@Override
 	public void sort(Integer[] array, int leftIndex, int rightIndex) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		int maior = maior(array,leftIndex,rightIndex);
+		int[] temp = new int[maior];
+		
+		//Inicializa
+		for(int i = 0; i < temp.length; i++){
+			temp[i] = 0;
+		}
+		
+		//Contagem
+		for (int i = leftIndex; i < rightIndex; i++) {
+			temp[array[i]] += 1;
+		}
+		
+		//Complementa
+		for (int i = leftIndex; i < maior; i++) {
+			temp[i] = temp[i] + temp[i - 1];
+
+		}
+		
+		//Ordena 
+		int[] b = new int[array.length];
+
+		for (int i = rightIndex; i > leftIndex; i--) {
+			b[temp[array[i]]] = array[i];
+			temp[array[i]] -= 1;
+		}
+		
+		//Muda array principal
+		for (int i = leftIndex; i < rightIndex; i++){
+			array[i] = b[i];
+		}
+	}
+
+	public static int maior(Integer[] array, int left, int rigth) {
+		int maior = 0;
+		for (int i = left; i < rigth; i++) {
+			if (array[i] > maior) {
+				maior = array[i];
+			}
+		}
+		return maior;
 	}
 
 }
